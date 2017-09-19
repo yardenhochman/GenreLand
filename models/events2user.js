4dc3f3e0341f56b51530b4c655c6a228243ee89f
Issac Grunwald <yitzygrunwald@gmail.com>
2017-09-19 12:03:40 -0400

    add results query to music2location table and add controller for results

--- models/events2user.js
+++ models/events2user.js
@@ -3,4 +3,5 @@ const db = require('../db/config');
 const Events2user = {};
 
 
+
 module.exports = Events2user;
--- models/music.js
+++ models/music.js
@@ -4,4 +4,12 @@ const Music = {};
 
 //This table will be populated on its own
 
+Music.insert = data => {
+    return db.one(`
+    INSERT INTO Music
+    (genre)
+    VALUES ($1)`,
+    [data.genre])
+}
+
 module.exports = Music;
