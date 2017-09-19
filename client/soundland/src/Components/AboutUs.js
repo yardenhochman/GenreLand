3b620352ff0136d16c1d15595e816dda5c240caf
Gary Chin <garychin100@gmail.com>
2017-09-18 22:26:36 -0400

    added about us page, needs pics of each person and small description

---
+++ client/soundland/src/Components/AboutUs.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class AboutUs {
+
+  render (){
+    return(
+        <div class="aboutus"><h2>About Team Yar-ss-ary-phanie</h2>
+            <div class="yarden"><h3>Yarden Hochman</h3>
+            <img src=""/>
+            <p></p>
+            </div>
+            <div class="issac"><h3>Issac Grunwald</h3>
+            <img src=""/>
+            <p></p>
+            </div>
+            <div class="gary"><h3>Gary Chin</h3>
+            <img src=""/>
+            <p>Born and raised in central NJ.  Graduated from Pace University with a BBA in Finance.</p>
+            </div>
+            <div class="stephanie"><h3>Stephanie Fung</h3>
+            <img src=""/>
+            <p></p>
+            </div>
+        </div>
+    );
+   }
+}
+
+export default AboutUs;
