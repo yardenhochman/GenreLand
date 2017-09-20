bf724cb298f4304a513d86227e20742e789b8e2c
Stephanie Fung <stephaniedfung@gmail.com>
2017-09-20 10:34:36 -0400

    Add and complete user auth

---
+++ controllers/user-controller.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/user.js');
+
+const usersController = {};
+
+usersController.index = (req, res) => {
+  res.json({
+    user: req.user,
+    data: 'Put a user profile on this route'
+  });
+}
+
+usersController.create = (req, res) => {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  User.create({
+    name: req.body.name,
+    username: req.body.username,
+    email: req.body.email,
+    password_digest: hash,
+  }).then(user => {
+    req.login(user, (err) => {
+      if (err) return next(err);
+      res.redirect('/profile');
+    });
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({error: err});
+  });
+}
+
+module.exports = usersController;
---
+++ routes/user-routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const profileRoutes = express.Router();
+
+//import controller
+//add routes
+
+//displays profile page
+profileRoutes.get('/', somecontroller)
+
+module.exports = profileRoutes;
