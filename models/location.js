238712dadb26a5b77585dc14dfda9c1ebab59b51
yarden hochman <yyyarden@gmail.com>
2017-09-20 10:08:56 -0400

    formated the data object on app.js better. added req.body.description for better use

--- models/location.js
+++ models/location.js
@@ -2,13 +2,13 @@ const db = require('../db/config');
 
 const Location = {};
 
-Location.insert = (zipcode) =>{
+Location.insert = (zipcode, description) =>{
     return db.one (`
         INSERT INTO location
         (zipcode, description)
         Values ($1, $2)
         RETURNING *`,
-        [zipcode, ' '])
+        [zipcode, description])
 };
 
 module.exports = Location;
