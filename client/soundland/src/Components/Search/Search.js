ba1690c665337020e5769b50327393b68d6bf5db
yarden hochman <yyyarden@gmail.com>
2017-09-22 13:13:22 -0400

    small tweaks

--- client/soundland/src/Components/Search/Search.js
+++ client/soundland/src/Components/Search/Search.js
@@ -14,98 +14,76 @@ class Search extends Component {
   constructor() {
     super();
     this.state = {
-      location: '',
-      music: "0",
-      waiting: false,
-      results: null
+      location:     '',
+      music:        "0",
+      waiting:      false,
+      results:      null,
+      venues:       null
     }
-    this.updateLocation = this.updateLocation.bind(this)
-    this.updateMusic = this.updateMusic.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
-    this.handleGenreChoice = this.handleGenreChoice.bind(this)
-    this.handleZipcodeInput = this.handleZipcodeInput.bind(this)
+    this.onSubmit =             this.onSubmit.bind(this)
+    this.handleGenreChoice =    this.handleGenreChoice.bind(this)
+    this.handleZipcodeInput =   this.handleZipcodeInput.bind(this)
   }
-    
-    handleZipcodeInput(event) {
-      event.preventDefault();
-      let input = Number(event.target.value);
-      if (isNaN(input))
-        return
-      this.updateLocation(input)
-    }
 
-
-    updateLocation(location) {
-      this.setState({
-        location: location
-      });
-    }
-
-    updateMusic(music) {
-      this.setState({
-        music: music
-      });
-    }
-    handleGenreChoice(event) {
-      event.preventDefault();
-      this.updateMusic(event.target.value)
-    }
-
-    onSubmit() {
-      if (this.state.location === null || this.state.music === "0")
-        return
-      this.setState({waiting: true})
-      let data = {
-        zipcode: this.state.location,
-        genre: Number(this.state.music),
-        description: ' '
-      };
-
-
+  handleZipcodeInput(event) {
+    event.preventDefault();
+    let input = Number(event.target.value);
+    if (isNaN(input))
+      return
+      this.setState({location: input});
+  }
+  handleGenreChoice(event) {
+    event.preventDefault();
+    this.setState({music: event.target.value});
+  }
+  onSubmit() {
+    if (this.state.location === null || this.state.music === "0")
+      return
+    this.setState({waiting: true})
+    let data = {
+      zipcode:      this.state.location,
+      genre:        Number(this.state.music),
+      description:  ' '
+    };
     axios({
-      method: 'POST',
-      url: 'http://localhost:3001/results',
+      method:      'POST',
+      url:         'http://localhost:3001/results',
       data
-    })
-    .then(res => {
-      console.log(res.data);
-      this.setState({
-        results:res.data,
-        waiting: false
-      })
-    })
-    .catch(err => {
-      console.log(err);
-    })
+    }).then( res => {
+        console.log(res.data);
+        this.setState({
+          results:res.data,
+          venues: res.venues,
+          waiting: false
+        })
+      }).catch( err => console.log(err))
   }
-
   render() {
     return (
       <div>
         <Header />
-        <div>
-          {/* <h1>You live in {this.state.location ? this.state.location.city : ''}, {this.state.location.state}</h1> */}
-          <Genreland 
-            name = {'Favorite Musical Artist'} 
-            updateMusic = {this.updateMusic} 
-            handleGenreChoice = {this.handleGenreChoice} 
-            selectedMusic = {this.state.music}
+          <div>
+            {/* <h1>You live in {this.state.location ? this.state.location.city : ''}, {this.state.location.state}</h1> */}
+            <Genreland 
+              name =                {'Favorite Musical Artist'} 
+              handleGenreChoice =   {this.handleGenreChoice} 
+              selectedMusic =       {this.state.music}
+              />
+            <Location 
+              name =                {'Where do you live'} 
+              handleZipcodeInput =  {this.handleZipcodeInput}
+              setLocation =         {this.state.location}
             />
-          <Location 
-            name = {'Where do you live'} 
-            updateLocation = {this.updateLocation} 
-            handleZipcodeInput = {this.handleZipcodeInput}
-            setLocation = {this.state.location}
-          />
-          <button 
-            onClick = {this.onSubmit}>
-            Submit
-          </button>
-          <Results 
-            results={this.state.results} 
-            waiting={this.state.waiting} 
-          />
-        </div>
+            <button 
+              onClick =             {this.onSubmit}>
+              Submit
+            </button>
+            <Results 
+              results =             {this.state.results} 
+              waiting =             {this.state.waiting} 
+              venues =              {this.state.venues}
+            />
+          </div>
         <Footer />
       </div>
     );
