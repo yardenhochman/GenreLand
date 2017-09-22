98fa89f864809c9ea6c3b0c96e4630828e64f56a
Gary Chin <garychin100@gmail.com>
2017-09-21 23:54:04 -0400

    change class to className

--- client/soundland/src/Components/AboutUs.js
+++ client/soundland/src/Components/AboutUs.js
@@ -11,17 +11,17 @@ class About extends Component {
       <div>
         <Header/>
 
-        <div class="about">
+        <div className="about">
           <h2>The SoundLand Team</h2>
 
-            <div class="team">
+            <div className="team">
               <h3>Gary Chin</h3>
               <img src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAmsAAAAJDExNzQ5NTZiLWM0NzItNGRiNy05N2VjLWNiZTRmMGRhODliNQ.jpg" alt="Gary Chin"/>
               <Link to={"https://github.com/Gnyc1001"}>Gary's GitHub Page</Link>
               <p>Born and raised in central NJ.  Graduated from Pace University with a BBA in Finance.</p>
             </div>
 
-            <div class="team">
+            <div className="team">
               <h3>Stephanie Fung</h3>
               <img src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAinAAAAJDQ3ZDBiNDNkLTBkMGQtNDhmMC1iMWNhLTVkYTRhYzZhOTVmZg.jpg" alt="Stephanie Fung"/>
               <Link to={"https://github.com/stephdfung"}>Stephanie's GitHub Page</Link>
@@ -29,14 +29,14 @@ class About extends Component {
             </div>
 
             </div>
-            <div class="team">
+            <div className="team">
               <h3>Issac Grunwald</h3>
               <img src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAXZAAAAJDJjYTMwMzNlLTkxMmMtNDM1Ni05MTJlLTI5M2I1ZWM5M2ZlOQ.jpg" alt="Issac Grunwald"/>
               <Link to={"https://github.com/issacg1"}>Issac's GitHub Page</Link>
               <p></p>
             </div>
 
-            <div class="team">
+            <div className="team">
               <h3>Yarden Hochman</h3>
               <img src="https://media.licdn.com/media/p/6/005/02a/234/316175c.jpg" alt="Yarden Hochman"/>
               <Link to={"https://github.com/yardenhochman"}>Yarden's GitHub Page </Link>
