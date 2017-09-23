021a284c007edb314eea58ee62351d1c79d61769
Stephanie Fung <stephaniedfung@gmail.com>
2017-09-23 18:21:21 -0400

    Update App.js to inclue loginrequired

--- client/soundland/src/App.js
+++ client/soundland/src/App.js
@@ -9,9 +9,31 @@ import Footer from './Components/Footer';
 import Login from './Components/Login';
 import Register from './Components/Registration';
 import User from './Components/User';
+import Success from './Components/Success';
+import Failure from './Components/Failure';
 
 //import Nav from './Nav';
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      auth: false
+    }
+    this.loggedIn = this.loggedIn.bind(this);
+    this.loginRequired = this.loginRequired.bind(this);
+  }
+
+  loggedIn = () =>{
+    this.setState({auth: true})
+    console.log(this.state.auth, 'this is the state of auth')
+  }
+
+  loginRequired = () =>{
+    if (this.state.auth === false) {
+      window.location.replace('/auth/login');
+    }
+  }
+
   render() {
     return (
       <Router>
@@ -19,9 +41,11 @@ class App extends Component {
           <Route exact path="/" component={Search} />
           <Route exact path="/about" component={About} />
           <Route exact path="/results" component={Results} />
-          <Route exact path="/auth/login" component={Login} />
+          <Route exact path="/auth/login" component={Login} loggedIn={this.loggedIn}/>
           <Route exact path="/auth/register" component={Register} />
-          <Route exact path="/user" component={User} />
+          <Route exact path="/user" component={User}/>
+          <Route exact path="/auth/success" component={Success}/>
+          <Route exact path="/auth/failure" component={Failure}/>
         </div>
       </Router>
     );
