66b11fd7952ecab1adda59848db80dffc4309cc0
yarden hochman <yyyarden@gmail.com>
2017-09-24 17:00:58 -0400

    moved the place api into a separate file

--- routes/results-routes.js
+++ routes/results-routes.js
@@ -5,8 +5,7 @@ const zipcodeHelper = require('../services/zipcode');
 
 
 //displays the results page
-resultsRoutes.post('/', resultsController.insertLocation, resultsController.insertMusic2Location, zipcodeHelper.getZipsAndPlaces, resultsController.queryResults)
-
+resultsRoutes.post('/', resultsController.insertLocation, resultsController.insertMusic2Location, zipcodeHelper.getZips, resultsController.queryResults)
 
 
 module.exports = resultsRoutes;
