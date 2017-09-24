f61929cf9fc6acae630f1018ef17ca5eb54b1514
yarden hochman <yyyarden@gmail.com>
2017-09-24 17:16:10 -0400

    separated zipcode and google places apis

--- routes/results-routes.js
+++ routes/results-routes.js
@@ -8,5 +8,4 @@ const zipcodeHelper = require('../services/zipcode');
 resultsRoutes.post('/', resultsController.insertLocation, resultsController.insertMusic2Location, zipcodeHelper.getZips, resultsController.queryResults)
 
 
-
 module.exports = resultsRoutes;
