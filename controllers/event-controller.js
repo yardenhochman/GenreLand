7ea8e626a5d42aebd50af7d17d17adf5b99fbc17
yarden hochman <yyyarden@gmail.com>
2017-09-24 22:53:57 -0400

    added fake events, successfully pulled event information from db into results

--- controllers/event-controller.js
+++ controllers/event-controller.js
@@ -22,6 +22,7 @@ eventController.EventsByZips = (req, res, next) =>{
     Events.FindByZips(res.locals.allZips)
     .then( rez => {
         res.locals.events = rez
+        console.log(rez)
         next()
         }).catch( err => console.log(err))
 }
